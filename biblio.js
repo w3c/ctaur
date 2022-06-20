4705e0c2d25b2d6e217085e6603b717725df159a
Jason White <jason@jasonjgw.net>
2022-06-20 13:02:02 -0400

    Add reference.

---
+++ biblio.js
@@ -0,0 +1,8 @@
+respecConfig.localBiblio = {
+  "concurrency-control": {
+    "title": "Concurrency control in groupware systems",
+    "authors": ["Clarence A. Ellis", "Simon J. Gibbs"],
+    "publisher": "Proceedings of the 1989 ACM SIGMOD international conference on Management of data",
+    "date": "1989"
+  }
+}
